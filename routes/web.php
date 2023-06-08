06e7106be86f059e9efacaef0e7a2f155fbcc2e2
Firdachs08 <91067475+Firdachs08@users.noreply.github.com>
2023-06-08 20:10:22 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\KaryawanController;
-use App\Http\Controllers\UsersController;
+use App\Http\Controllers\KaryawanController;
 use App\Http\Controllers\StokController;
 
 /*
@@ -37,12 +36,16 @@ Route::get('/dash-pesanan', [App\Http\Controllers\PesananController::class, 'ind
 Route::get('/dash-stok', [App\Http\Controllers\StokController::class, 'index'])->name('dash-stok');
 Route::get('/add-stok', [App\Http\Controllers\StokController::class, 'create'])->name('add-stok');
 Route::post('/save-stok', [App\Http\Controllers\StokController::class, 'store'])->name('save-stok');
+Route::post('/hapus-stok/{id}', [App\Http\Controllers\StokController::class, 'destroy'])->name('hapus-stok');
+Route::post('/update-stok/{id}', [App\Http\Controllers\KaryawanController::class, 'update'])->name('update-stok');
+//Route::put('/add-stok/{id}', [App\Http\Controllers\StokController::class, 'update'])->name('stok.update');
 
 //route karyawan
 Route::get('/dash-karyawan', [App\Http\Controllers\KaryawanController::class, 'index'])->name('dash-karyawan');
-//Route::get('/add-karyawan', [App\Http\Controllers\KaryawanController::class, 'create'])->name('add-karyawan');
-//Route::post('/simpan-karyawan', [App\Http\Controllers\KaryawanController::class, 'store'])->name('simpan-karyawan');
-//Route::get('/edit-karyawan', [App\Http\Controllers\KaryawanController::class, 'edit'])->name('edit-karyawan');
+Route::get('/add-karyawan', [App\Http\Controllers\KaryawanController::class, 'create'])->name('add-karyawan');
+Route::post('/simpan-karyawan', [App\Http\Controllers\KaryawanController::class, 'store'])->name('simpan-karyawan');
+Route::post('/hapus-karyawan/{id}', [App\Http\Controllers\KaryawanController::class, 'destroy'])->name('hapus-karyawan');
+Route::get('/edit-karyawan', [App\Http\Controllers\KaryawanController::class, 'edit'])->name('edit-karyawan');
 
 
 
