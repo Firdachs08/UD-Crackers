6c6ec710c3e4e7e3396700e94b0f5477bf231a0c
Firdachs08 <91067475+Firdachs08@users.noreply.github.com>
2023-06-21 13:43:45 +0700

    upload

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::post('/register', [ApiAuthController::class, 'register']);
 Route::post('/login', [ApiAuthController::class, 'login']);
 Route::get('/stok', [ApiStokController::class, 'all']);
 Route::post('/pesanan', [ApiPesananController::class, 'insert'])->middleware('auth:sanctum');
-Route::post('/pesanan-show', [ApiPesananController::class, 'show'])->middleware('auth:sanctum');
+Route::get('/pesanan-show', [ApiPesananController::class, 'show'])->middleware('auth:sanctum');
 Route::get('/me', [ApiAuthController::class, 'userlogin'])->middleware('auth:sanctum');
 Route::post('/logout', [ApiAuthController::class, 'logout'])->middleware('auth:sanctum');
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
